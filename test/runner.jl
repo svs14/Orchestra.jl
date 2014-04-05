34a60e0f1c6fbdeb9e3893f5e301a27cf440142a
Samuel Jenkins <jenkins.j.samuel@gmail.com>
2014-04-05 19:50:42 +1100

    Reorder of tests.

--- test/runner.jl
+++ test/runner.jl
@@ -5,8 +5,8 @@ module TestRunner
     include("test_abstractlearner.jl")
     include("test_util.jl")
     include("learners/julia/test_decisiontree.jl")
-    include("learners/julia/test_ensemble.jl")
     include("learners/julia/test_libsvm.jl")
+    include("learners/julia/test_ensemble.jl")
     include("learners/julia/test_selection.jl")
 
     exitstatus()
